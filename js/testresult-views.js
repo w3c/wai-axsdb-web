9ffe523b0f779946c64e4b1e053e4038e66ebfbe
Eric Eggert <ee@w3.org>
2014-06-03 08:36:55 +0200

    TestCases in Browse Test Results table

--- js/testresult-views.js
+++ js/testresult-views.js
@@ -8,6 +8,48 @@
  * }
  *
  */
+
+testcaseData = {
+  "entity": [
+    {
+      "noOfAll": "2",
+      "noOfPass": "1",
+      "testcaseId": "H49_0000073",
+      "testcaseTitle": "Em and strong elements can be used to stress parts of a text"
+    },
+    {
+      "noOfAll": "3",
+      "noOfPass": "1",
+      "testcaseId": "H49_0000074",
+      "testcaseTitle": "sub elements indicate subscript in HTML"
+    },
+    {
+      "noOfAll": "4",
+      "noOfPass": "1",
+      "testcaseId": "H49_0000075",
+      "testcaseTitle": "sup elements indicate superscript in HTML"
+    },
+    {
+      "noOfAll": "5",
+      "noOfPass": "2",
+      "testcaseId": "H49_0000076",
+      "testcaseTitle": "abbr and acronym can identify special text in html"
+    },
+    {
+      "noOfAll": "6",
+      "noOfPass": "3",
+      "testcaseId": "H49_0000077",
+      "testcaseTitle": "b, i and u can identify special text in HTML"
+    },
+    {
+      "noOfAll": "7",
+      "noOfPass": "5",
+      "testcaseId": "H49_0000078",
+      "testcaseTitle": "small identifies special text in HTML"
+    }
+  ]
+};
+
 accessdb.Views.TestResultsDataOverview = function (){
     this.$el = $("#TestResultsDataOverviewDiv");
     this.results = null;
@@ -15,6 +57,24 @@ accessdb.Views.TestResultsDataOverview = function (){
         if(this.results){
             var template = _.template( $("#TestResultsDataOverview_template").html(), {results: this.results} );
             this.$el.html( template );
+            this.$el.find('button').on('click', function(){
+              el = $(this);
+              eltbody = el.parents('tbody');
+              sistertbody = eltbody.next(); // tbody.testcases
+              icon = el.find('.icon');
+              iconlabel = icon.find('.visuallyhidden');
+              eltbody.toggleClass('collapsed');
+              console.log(testcaseData);
+              var tctemplate = _.template( $("#TestResultsDataOverviewTestCases_template").html(), {results: testcaseData} );
+              sistertbody.html( tctemplate );
+              icon.toggleClass('icon-collapse icon-expand');
+              if (icon.is('.icon-expand')) {
+                iconlabel.text('Expand');
+              } else {
+                iconlabel.text('Collapse');
+              }
+            });
+
         }
     };
     this.fetch = function (filter, callback){
@@ -45,3 +105,4 @@ accessdb.Views.TestResultsDataOverview = function (){
         }
     }
 };
+
